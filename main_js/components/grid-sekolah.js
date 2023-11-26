8b778d825d5b5e40112481b5af60d0ca22806b56
Fahmi Dzulqarnain <fdz.fhz1302@gmail.com>
2023-11-26 18:04:56 +0800

    Add option to hide school

--- main_js/components/grid-sekolah.js
+++ main_js/components/grid-sekolah.js
@@ -21,56 +21,61 @@ function setGridSekolahContent(path, jsonData) {
 				const namaSekolah = sekolah.namaSekolah
 				const logo = sekolah.logo
 				const detail = sekolah.detail
+				const isShowOnTheWeb = sekolah.isShowOnTheWeb
 
-				if (detail.length > 1) {
-					const modalTitle = document.getElementById("modal-title")
-					const modalContent = document.getElementById("modal-content")
-					const requestButton = new XMLHttpRequest()
-
-					requestButton.open("GET", "assets/components/location-button.html")
-					requestButton.send()
-					requestButton.onload = function () {
-						if (requestButton.status == 200) {
-							const buttonView = requestButton.responseText
-    
-							buttonSekolah[i].onclick = function () {
-								setToImage("modal-img", logo)
-								modalTitle.innerHTML = namaSekolah
-                                modalContent.innerHTML = ""
-                                
-    							for (let index = 0; index < detail.length; index++) {
-    								modalContent.innerHTML += buttonView
-    							}
-    
-    							const buttonLocation = document.getElementsByName("btn-location")
-    
-								for (let index = 0; index < detail.length; index++) {
-									buttonLocation[index].innerHTML = detail[index].alamat
+				if (isShowOnTheWeb) {
+					if (detail.length > 1) {
+						const modalTitle = document.getElementById("modal-title")
+						const modalContent = document.getElementById("modal-content")
+						const requestButton = new XMLHttpRequest()
+	
+						requestButton.open("GET", "assets/components/location-button.html")
+						requestButton.send()
+						requestButton.onload = function () {
+							if (requestButton.status == 200) {
+								const buttonView = requestButton.responseText
+		
+								buttonSekolah[i].onclick = function () {
+									setToImage("modal-img", logo)
+									modalTitle.innerHTML = namaSekolah
+									modalContent.innerHTML = ""
+									
+									for (let index = 0; index < detail.length; index++) {
+										modalContent.innerHTML += buttonView
+									}
+		
+									const buttonLocation = document.getElementsByName("btn-location")
+		
+									for (let index = 0; index < detail.length; index++) {
+										buttonLocation[index].innerHTML = detail[index].alamat
+									}
+	
+									for (let index = 0; index < detail.length; index++) {
+										const selectedDetail = detail[index]
+										const idSekolah = selectedDetail.idSekolah
+		
+										buttonLocation[index].onclick = function () {
+											localStorage.setItem("idSekolah", idSekolah)
+											location.href = "main.html"
+										}
+									}
+	
+									openDialog()
 								}
-
-    							for (let index = 0; index < detail.length; index++) {
-    								const selectedDetail = detail[index]
-    								const idSekolah = selectedDetail.idSekolah
-    
-    								buttonLocation[index].onclick = function () {
-    									localStorage.setItem("idSekolah", idSekolah)
-    									location.href = "main.html"
-    								}
-    							}
-
-								openDialog()
 							}
 						}
+					} else {
+						buttonSekolah[i].onclick = function () {
+							localStorage.setItem("idSekolah", detail[0].idSekolah)
+							location.href = "main.html"
+						}
 					}
+	
+					txtNamaSekolah[i].innerHTML = namaSekolah
+					imgLogoSekolah[i].src = logo
 				} else {
-					buttonSekolah[i].onclick = function () {
-						localStorage.setItem("idSekolah", detail[0].idSekolah)
-						location.href = "main.html"
-					}
+					buttonSekolah[i].style.display = "none"
 				}
-
-				txtNamaSekolah[i].innerHTML = namaSekolah
-				imgLogoSekolah[i].src = logo
 			}
 		}
 	}
