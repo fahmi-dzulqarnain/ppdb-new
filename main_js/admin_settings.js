1494985523c8f7f96b8ca49ecf92daa86cd93dbd
Fahmi Dzulqarnain <fdz.fhz1302@gmail.com>
2023-12-15 07:50:01 +0800

    Fix Setting Bug

--- main_js/admin_settings.js
+++ main_js/admin_settings.js
@@ -1,3 +1,54 @@
+const request = new XMLHttpRequest()
+const accessToken = localStorage.getItem('accessToken')
+const idSekolah = localStorage.getItem('idSekolah')
+const query = `?id=${idSekolah}`
+
+if (accessToken == null) {
+    signOut()
+}
+
+request.onload = function () {
+    const response = JSON.parse(request.responseText)
+    const statusCode = this.status
+
+    if (statusCode == 401) {
+        signOut()
+    } else if (statusCode != 200) {
+        const title = response.error
+        const messages = response.message
+
+        attachToView('titleDialog', title)
+        attachToView('descriptionDialog', messages)
+        openDialog()
+    } else {
+        const checkboxRegistration = document.getElementById('checkboxRegistration')
+        const checkboxShown = document.getElementById('checkboxShown')
+        const data = response
+        console.log(data)
+
+        setInputValue('txtNamaSekolah', data.namaSekolah)
+        setInputValue('txtAlamat', data.alamat)
+        setInputValue('txtNamaRekening', data.namaRekening)
+        setInputValue('txtNoRekening', data.noRekening)
+        setInputValue('txtBiayaAwal', numberWithSeparators(data.biayaAwal))
+        setInputValue('txtBiayaSPP', numberWithSeparators(data.biayaSPP))
+        setInputValue('txtBiayaPendaftaran', numberWithSeparators(data.biayaPendaftaran))
+        setInputValue('txtKontak', data.kontak)
+        setInputValue('txtLinkGrupWA', data.linkWAGroup)
+
+        if (data.isRegistrationOpen) {
+            checkboxRegistration.checked = true
+        }
+
+        if (data.isShowOnTheWeb) {
+            checkboxShown.checked = true
+        }
+    }
+}
+
+request.open('GET', `${mainURL}sekolah/byID${query}`)
+request.send()
+
 function saveSettings() {
     const accessToken = localStorage.getItem('accessToken')
     const request = new XMLHttpRequest()
@@ -13,7 +64,7 @@ function saveSettings() {
     const checkboxShown = document.getElementById('checkboxShown')
     const isRegistrationOpen = checkboxRegistration.checked
     const isShowOnTheWeb = checkboxShown.checked
-    
+
     const jsonBody = JSON.stringify({
         namaRekening,
         noRekening,
@@ -26,7 +77,7 @@ function saveSettings() {
         linkWAGroup
     })
 
-    request.onload = function() {
+    request.onload = function () {
         const statusCode = this.status
 
         if (statusCode != 200) {
@@ -36,7 +87,7 @@ function saveSettings() {
                 const response = JSON.parse(this.responseText)
                 const title = response.error
                 const messages = response.message
-    
+
                 attachToView('titleDialog', title)
                 attachToView('descriptionDialog', messages)
                 openDialog()
